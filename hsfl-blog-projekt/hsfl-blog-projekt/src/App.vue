2b965da34b60baf09034ad51f2d38e0844aa43d4
VencedorParis <sergey.hellwig@stud.hs-flensburg.de>
2023-08-10 13:48:17 +0200

    add Navbar

--- hsfl-blog-projekt/hsfl-blog-projekt/src/App.vue
+++ hsfl-blog-projekt/hsfl-blog-projekt/src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+     <h1>HSFL-Blog</h1>
+     <p>Blog der Hochschule Flensburg</p>
+   </div>
+ 
+ <nav>
+   <div>
+   <router-link to="/">Startseite</router-link> |
+   <router-link to="/profile">Mein Account</router-link> |
+   <router-link to="/bibliothek">Bibliothek</router-link> 
+   </div>
+   <router-link to="/logging">Log Out</router-link>
+
+ </nav>
+ <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+}
+
+nav {
+ 
+ display: flex; 
+ justify-content: space-between;
+ align-items: center;
+ padding: 15px;
+ background-color: #bad0f5;
+ text-align: left;
+}
+
+nav a {
+ font-weight: bold;
+ color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+ color: #bf654e;
+}
+.header h1 {
+   font-size: 50px;
+   margin-bottom: 5px; 
+   margin-top: px;
+ }
+ .header {
+   margin-top: 0;
+   padding: 2px;
+   text-align: center;
+   background: white;
+   color: black;
+   
+ }
+ .header p {
+ margin-top: 0;
+
 }
 </style>
